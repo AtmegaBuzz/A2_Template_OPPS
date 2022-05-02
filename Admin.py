82dafc47a22370e581a7b02f5ad7bc9ccffd4267
AtmegaBuzz <swapnilshinde9382@gmail.com>
2022-05-02 10:33:50 +0530

    a

--- Admin.py
+++ Admin.py
@@ -14,9 +14,22 @@ class Admin(User):
         filename = "data/course_data/raw_data2.txt"
         with open(filename,'r') as f:
             raw_data = f.read()
-        course_data = json.loads(raw_data)["unit"]
-        print(course_data.keys())
-        pass
+        course_data = json.loads(raw_data)["unit"]["items"]
+
+        with open('courses.txt','a') as f:
+            
+            for course in course_data:
+                course_id = course.get("id")
+                course_title = course.get("title")
+                image_100x100 = course.get("image_100x100")
+                headline = course.get("headline").replace("\n"," ")
+                num_of_subscribers = course.get("num_subscribers")
+                avg_rating = course.get("avg_rating")
+                course_content_length = course.get("content_info").replace("total hours","")
+
+                f.writelines(f"{course_id};;;{course_title};;;{image_100x100};;;{headline};;;{num_of_subscribers};;;{avg_rating};;;{course_content_length}\n")
+
+                   
 
     def extract_review_info(self):
         filenames = glob.glob("data/review_data/*.json")
@@ -31,12 +44,12 @@ class Admin(User):
             with open('review.txt','a') as f:
                 
                 for review in reviews:
-                    course_id = review['id']
-                    review_content = review['content']
-                    review_rating = review['rating']
-                    review_id = review['user']['id']
+                    course_id = review.get('id')
+                    review_content = review.get('content').replace("\n"," ")
+                    review_rating = review.get('rating')
+                    review_id = review.get('user').get('id')
 
-                    f.write(f"{review_id};;;{review_content};;;{review_rating};;;{course_id}")
+                    f.write(f"{review_id};;;{review_content};;;{review_rating};;;{course_id}\n")
             
     def extract_students_info(self):
         filenames = glob.glob("data/review_data/*.json")
@@ -49,19 +62,41 @@ class Admin(User):
 
             with open('user_student.txt','a') as f:
                 for review in reviews:
-                    user_id = review['user']['id']
-                    username = review['user']['name'].lower().replace(" ","_")
-                    user_title = review['user']['title']
-                    user_image = review['user']['image_50x50']
-                    user_initials = review['user']['initials']
-                    review_id = review['id']
+                    user_id = review.get('user').get('id')
+                    username = review.get('user').get('name').lower().replace(" ","_")
+                    user_title = review.get('user').get('title')
+                    user_image = review.get('user').get('image_50x50')
+                    user_initials = review.get('user').get('initials')
+                    review_id = review.get('id')
                     password = f"{user_initials}{user_id}{user_initials}"
 
-                    f.write(f"{user_id};;;{username};;;{password};;;{user_title};;;{user_image};;;{user_initials};;;{review_id}")
+                    f.write(f"{user_id};;;{username};;;{password};;;{user_title};;;{user_image};;;{user_initials};;;{review_id}\n")
                  
 
     def extract_instructor_info(self):
-        pass
+        filename = "data/course_data/raw_data2.txt"
+        with open(filename,'r') as f:
+            raw_data = f.read()
+        course_data = json.loads(raw_data)["unit"]["items"]
+
+        with open('courses.txt','a') as f:
+            
+            for course in course_data:
+                instrustors = course.get("visible_instructors",[])
+                course_id = course.get('id')
+                
+                 
+
+                for instrustor in instrustors:
+                    instrustor_id = instrustor.get("id")
+                    username = instrustor.get("title")
+                    password = instrustor.get("image_100x100")
+                    display_name = instrustor.get("headline").replace("\n"," ")
+                    job_title = instrustor.get("num_subscribers")
+                    image_100x100 = instrustor.get("avg_rating")
+                    course_content_length = instrustor.get("content_info").replace("total hours","")
+
+                    f.writelines(f"{course_id};;;{course_title};;;{image_100x100};;;{headline};;;{num_of_subscribers};;;{avg_rating};;;{course_content_length}\n")
 
     def extract_info(self):
         self.extract_course_info()
@@ -96,4 +131,4 @@ class Admin(User):
 
 
 admin = Admin()
-admin.extract_course_info()
+admin.extract_students_info()
